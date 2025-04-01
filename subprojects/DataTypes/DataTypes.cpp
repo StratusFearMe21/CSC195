aa4c1cc2c669d1619a80c5f37d89e6670bd02ee3
Isaac Mills <rooster0055@protonmail.com>
2025-04-01 15:48:07 -0600

    Complete DataTypes assignment with attempt at input validation

--- subprojects/DataTypes/DataTypes.cpp
+++ subprojects/DataTypes/DataTypes.cpp
@@ -1,7 +1,61 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-int main() {
+const float TAX = 0.1f;
 
+#define PROMPT(prompt, output)                                                 \
+  for (;;) {                                                                   \
+    cout << prompt;                                                            \
+    if (cin >> output) {                                                       \
+      break;                                                                   \
+    } else {                                                                   \
+      cout << "The value entered wasn't valid" << endl;                        \
+      cin.clear();                                                             \
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');                     \
+    }                                                                          \
+  }
+
+int main() {
+  string name = "";
+  char initial = '\0';
+  short age = -1;
+  bool isAdult = -1;
+  unsigned int zipcode = 0;
+  float wage = -1.0;
+  short daysWorked = -1;
+  float hoursWorkedPerDay[7] = {-1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0};
+
+  PROMPT("Enter first name: ", name);
+  PROMPT("Enter initial of last name: ", initial);
+  PROMPT("Enter age: ", age);
+  PROMPT("Enter zipcode: ", zipcode);
+  PROMPT("Enter hourly wage: ", wage);
+  PROMPT("Enter number of days worked (max 7): ", daysWorked);
+
+  isAdult = (age >= 18); // Determine adulthood status
+  float totalHours = 0;  // Initialize total hours to 0
+
+  for (int i = 0; i < daysWorked; i++) {
+    cout << "Enter hours worked for day " << (i + 1) << ": ";
+    cin >> hoursWorkedPerDay[i];
+
+    totalHours += hoursWorkedPerDay[i]; // Accumulate total hours
+  }
+
+  float grossIncome = totalHours * wage;
+  float taxAmount = grossIncome * TAX;
+  float netIncome = grossIncome - taxAmount;
+
+  cout << "\nEmployee Payroll Summary\n";
+  cout << "-------------------------\n";
+  cout << "Name: " << name << " " << initial << ".\n";
+  cout << "Age: " << age << " (Adult: " << (isAdult ? "Yes" : "No") << ")\n";
+  cout << "Zipcode: " << zipcode << "\n";
+  cout << "Hourly Wage: $" << wage << " per hour\n";
+  cout << "Total Hours Worked: " << totalHours << " hours\n";
+  cout << "Gross Income: $" << grossIncome << "\n";
+  cout << "Tax Amount: $" << taxAmount << "\n";
+  cout << "Net Income: $" << netIncome << "\n";
 }
