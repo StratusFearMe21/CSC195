0b09ae67143c97dec85ad7e9f9c0bbc1e42a5c3f
Isaac Mills <rooster0055@protonmail.com>
2025-04-01 14:59:40 -0600

    Added Git message to csc195.cpp

--- csc195.cpp
+++ csc195.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+  cout << "Hello, World!" << endl;
+  cout << "Git is now tracking this project!" << endl;
+  return 0;
 }
